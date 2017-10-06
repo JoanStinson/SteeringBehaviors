50cb7ac203cc07cae649ee5efd725dc44487d69b
Toni Ferrari <toniferrari87@hotmail.com>
2017-10-06 18:45:35 +0200

    REVISAR DISTANCIA I SUBDISTANCIA DEL COLLISION AVOIDANCE

--- src/SteeringBehavior.cpp
+++ src/SteeringBehavior.cpp
@@ -4,7 +4,7 @@
 
 SteeringBehavior::SteeringBehavior()
 {
-	
+
 }
 
 
@@ -34,7 +34,7 @@ Vector2D SteeringBehavior::Flee(Agent *agent, Vector2D target, float dtime)
 	return SteeringForce * agent->getMaxForce();
 }
 
-Vector2D SteeringBehavior::Arrive(Agent *agent, Vector2D target, float r,float dtime) {
+Vector2D SteeringBehavior::Arrive(Agent *agent, Vector2D target, float r, float dtime) {
 	Vector2D desiredV = target - agent->getPosition(); // agafam el vector que resulta de restar posició on volem anar - posició actual. Resultat= velocitat adecuada
 	Vector2D steeringForce = desiredV - agent->getVelocity();
 	float factor;
@@ -48,7 +48,7 @@ Vector2D SteeringBehavior::Arrive(Agent *agent, Vector2D target, float r,float d
 		steeringForce *= agent->getMaxForce();
 	}
 	else {
-		factor = dist.Length() / r; 
+		factor = dist.Length() / r;
 		draw_circle(TheApp::Instance()->getRenderer(), target.x, target.y, r, 0, 255, 0, 1);
 		desiredV *= factor;
 	}
@@ -56,7 +56,7 @@ Vector2D SteeringBehavior::Arrive(Agent *agent, Vector2D target, float r,float d
 	return steeringForce;
 }
 
-Vector2D SteeringBehavior::Pursue(Agent *agent, Vector2D target,float maxSpeed, float dtime)
+Vector2D SteeringBehavior::Pursue(Agent *agent, Vector2D target, float maxSpeed, float dtime)
 {
 
 	Vector2D desiredV = target - agent->getPosition(); // agafam el vector que resulta de restar posició on volem anar - posició actual. Resultat= velocitat adecuada
@@ -83,7 +83,7 @@ Vector2D SteeringBehavior::Evade(Zombie *agent, Vector2D target, float maxSpeed,
 Vector2D SteeringBehavior::Wander(Agent *agent, Vector2D target, float dtime)
 {
 
-	Vector2D desiredV = target - agent->getPosition(); 
+	Vector2D desiredV = target - agent->getPosition();
 	desiredV = desiredV.Normalize();
 	desiredV *= agent->getMaxVelocity();
 	Vector2D steeringForce = desiredV - agent->getVelocity();
@@ -93,45 +93,72 @@ Vector2D SteeringBehavior::Wander(Agent *agent, Vector2D target, float dtime)
 	return steeringForce;
 }
 
-Vector2D SteeringBehavior::CollisionAvoidance(Agent *agent, Vector2D target,Vector2D avoidForce, std::vector<Vector2D> obstacles, std::vector<Vector2D> distances, std::vector<Vector2D> subdistances, float dtime, float MAX_AHEAD, float MAX_AVOID_FORCE, float v)
+Vector2D SteeringBehavior::CollisionAvoidance(Agent *agent, Vector2D target, Vector2D avoidForce, std::vector<Vector2D> obstacles, std::vector<Vector2D> distances, std::vector<Vector2D> subdistances, float dtime, float MAX_AHEAD, float MAX_AVOID_FORCE, float v)
 {
 	v = agent->getVelocity().Length() / agent->getMaxVelocity();
-	Vector2D ahead = agent->getPosition() + agent->getVelocity().Normalize() * v; // aixo va a mitjes. Ahead es un vector que va de 0 a 1. Per lo que només hi veu a un pixel de distancia
-	Vector2D halfahead = agent->getPosition() + agent->getVelocity().Normalize() * v * 0.5;
-	
+	Vector2D ahead = agent->getPosition() + agent->getVelocity()* v; // al deixar de normalitzar sa velocitat ha funcionat
+	Vector2D halfahead = agent->getPosition() + agent->getVelocity()*  0.5 *v;
+
 	for (int i = 0; i < obstacles.size(); i++) {
-		Vector2D d = obstacles[i] - ahead;
+
+		Vector2D d = ahead - obstacles[i];
 		distances.push_back(d);
-		Vector2D subD = obstacles[i] - halfahead;
+		Vector2D subD = halfahead - obstacles[i];
 		subdistances.push_back(subD);
-	};
 
-	if (distances.size() != 0) {
+	}
+	//MIRAR DISTANCIA I SUBDISTANCIA////
+	for (int i = 0; i <distances.size(); i++) {
 
-		for (int i = 0; i < distances.size(); i++) {
-			if (distances[i].Length() <= 50 || subdistances[i].Length() <= 50 ) {//50 = radi de s'esfera
+		draw_circle(TheApp::Instance()->getRenderer(), 50, 50, 10, 255, 255, 255, 255);
+		SDL_RenderDrawLine(TheApp::Instance()->getRenderer(), 100, 20 * i + 20, distances[i].Length(), 20 * i + 20);
+	}
 
-				avoidForce = ahead - obstacles[i];
-				avoidForce = avoidForce.Normalize()*MAX_AVOID_FORCE;
-			}
-			else
-				avoidForce = Vector2D(0, 0);
+
+	for (int i = 0; i < obstacles.size(); i++) {
+
+		if (distances[i].Length() <= 50 || subdistances[i].Length() <= 50 || agent->getPosition().Distance(agent->getPosition(), obstacles[i]) <= 50) {//50 = radi de s'esfera
+
+			avoidForce = ahead - obstacles[i];
+			avoidForce = avoidForce.Normalize()*MAX_AVOID_FORCE;
+			draw_circle(TheApp::Instance()->getRenderer(), 50, 50, 10, 255, 255, 255, 255);
+			SDL_RenderDrawLine(TheApp::Instance()->getRenderer(), agent->getPosition().x, agent->getPosition().y, avoidForce.x, avoidForce.y);
+			Vector2D desiredV = target - agent->getPosition();
+			desiredV = desiredV.Normalize();
+			desiredV *= agent->getMaxVelocity();
+			Vector2D steeringForce = desiredV - agent->getVelocity();
+			steeringForce /= agent->getMaxVelocity();
+			steeringForce *= agent->getMaxForce();
+			distances.clear();
+			subdistances.clear();
+
+			draw_circle(TheApp::Instance()->getRenderer(), 50, 50, 10, 255, 255, 255, 255);
+
+			SDL_RenderDrawLine(TheApp::Instance()->getRenderer(), agent->getPosition().x, agent->getPosition().y, ahead.x, ahead.y);
+
+
+			return steeringForce + avoidForce;
+
+		}
+		else {
+
+			Vector2D desiredV = target - agent->getPosition();
+			desiredV = desiredV.Normalize();
+			desiredV *= agent->getMaxVelocity();
+			Vector2D steeringForce = desiredV - agent->getVelocity();
+			steeringForce /= agent->getMaxVelocity();
+			steeringForce *= agent->getMaxForce();
+
+
+			draw_circle(TheApp::Instance()->getRenderer(), 50, 50, 10, 255, 255, 255, 255);
+
+			SDL_RenderDrawLine(TheApp::Instance()->getRenderer(), agent->getPosition().x, agent->getPosition().y, ahead.x, ahead.y);
+
+
+			return steeringForce;
+
 		}
 	}
-	Vector2D desiredV = target - agent->getPosition();
-	desiredV = desiredV.Normalize();
-	desiredV *= agent->getMaxVelocity();
-	Vector2D steeringForce = desiredV - agent->getVelocity();
-	steeringForce /= agent->getMaxVelocity();
-	steeringForce *= agent->getMaxForce();
-	distances.clear();
-	subdistances.clear();
 
-	draw_circle(TheApp::Instance()->getRenderer(),50,50, v, 255,255, 255, 255);
-
-	SDL_RenderDrawLine(TheApp::Instance()->getRenderer(), agent->getPosition().x, agent->getPosition().y, ahead.x, ahead.y);
-
-
-	return steeringForce + avoidForce;
 }
 
